d6b357cf01e1f7261ce4e29d2a8b6845546bbac5
HenrikJannsen <boilingfrog@gmx.com>
2025-04-08 12:08:19 +0700

    Set targetCompatibility to Java 11.
    New devs have reported that they could not get Bisq 2 running without having Java 11 installed. I cannot verify that this is really required, but by setting the sourceCompatibility and targetCompatibility to Java 11 this should help to avoid that issue.

--- build-logic/commons/src/main/kotlin/bisq.java-conventions.gradle.kts
+++ build-logic/commons/src/main/kotlin/bisq.java-conventions.gradle.kts
@@ -13,6 +13,8 @@ java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(11))
     }
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
 }
 
 tasks {
